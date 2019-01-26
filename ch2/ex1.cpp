16aea0207d0f820f32cf67ce48b5f8f57331c21d
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-01-26 00:37:58 -0500

    some of ch2

---
+++ ch2/ex1.cpp
@@ -0,0 +1,15 @@
+// What does this do?
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while (i < 10) {
+        i += 1;
+        cout << i << endl;
+    }
+
+    return 0;
+}
